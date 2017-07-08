f4922acb1b6732d4b33bc137f0c90a366fae22a0
RouxRC <b.ooghe@gmail.com>
2017-07-08 22:22:00 +0200

    add autoupdate script

---
+++ build_gif.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+last=$(ls shots | tail -1)
+new=$(date +%y%m%d).png
+
+if [ "$last" = "$new" ]; then
+  exit 0
+fi
+
+wget "https://www.nosdeputes.fr/graphes/groupes/all?mapId=Map_0000.map" -O shots/$new
+
+if diff shots/$last shots/$new | grep diff > /dev/null; then
+  convert -delay 100 -loop 0 shots/*.png synthese.gif
+  git add shots synthese.gif
+  git commit -m "autoupdate"
+  git push
+else
+  rm -f shots/$new
+fi
