848070d414192f503bc1481852a84964dc1717ec
RouxRC <b.ooghe@gmail.com>
2017-07-08 22:28:37 +0200

    silent wget

--- build_gif.sh
+++ build_gif.sh
@@ -7,7 +7,7 @@ if [ "$last" = "$new" ]; then
   exit 0
 fi
 
-wget "https://www.nosdeputes.fr/graphes/groupes/all?mapId=Map_0000.map" -O shots/$new
+wget -q "https://www.nosdeputes.fr/graphes/groupes/all?mapId=Map_0000.map" -O shots/$new
 
 if diff shots/$last shots/$new | grep diff > /dev/null; then
   convert -delay 100 -loop 0 shots/*.png synthese.gif
