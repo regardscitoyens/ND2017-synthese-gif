9adac7a6de337ffc93f17804ab9f0d0404d3d401
Nicolas Joyard <nicolas@njoyard.fr>
2017-08-24 13:05:12 +0200

    Ajout date en légende + crop bordure transparente

--- build_gif.sh
+++ build_gif.sh
@@ -11,8 +11,17 @@ fi
 
 wget -q "https://www.nosdeputes.fr/graphes/groupes/all?mapId=Map_0000.map" -O shots/$new
 
+mkdir -p captioned
+for shot in shots/*.png; do
+  captioned=captioned/$(basename $shot)
+  if [ ! -f "$captioned" ]; then
+    label=$(date -d ${shot:6:6} +"%d %B %Y")
+    convert $shot -crop 707x323+7+7 -background "#F0F0F0" label:"$label" -gravity Center -append $captioned
+  fi
+done
+
 if diff shots/$last shots/$new | grep diff > /dev/null; then
-  convert -delay 100 -loop 0 shots/*.png synthese.gif
+  convert -delay 100 -loop 0 captioned/*.png synthese.gif
   git add shots synthese.gif
   git commit -m "autoupdate"
   git push
